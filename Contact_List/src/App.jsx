3e5b9d0208ccd0b3dc5b8940fcf65b594748b408
Annorinoo <annjenelr@gmail.com>
2023-08-15 16:45:57 -0700

    finalized

--- Contact_List/src/App.jsx
+++ Contact_List/src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import ContactList from './components/ContactList'
+import React, { useState } from "react";
+import ContactList from "./components/ContactList";
+import SelectedContact from "./components/SelectedContact";
 
 export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
+
   return (
     <>
-      <ContactList />
+      {selectedContactId ? (
+        <div>Selected Contact View</div>
+      ) : (
+        <ContactList setSelectedContactId={setSelectedContactId} />
+      )}
     </>
   );
 }
-
--- Contact_List/src/components/ContactRow.jsx
+++ Contact_List/src/components/ContactRow.jsx
@@ -1,9 +1,15 @@
-export default function ContactRow({ contact }) {
+import React from "react";
+
+export default function ContactRow({ setSelectedContactID, contact }) {
     return (
-        <tr>
-            <td>{contact.name}</td>
-            <td>{contact.email}</td>
-            <td>{contact.phone}</td>
+        <tr
+            onClick={() => {
+                setSelectedContactID(contact.id);
+                }}
+                >
+                <td>{contact.name}</td>
+                <td>{contact.email}</td>
+                <td>{contact.phone}</td>
         </tr>
     )
 }
